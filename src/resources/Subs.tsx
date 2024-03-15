243a5aa94dc3f786836f2cede7c84dd248ae365b
KhusheRanjan <khusheranjan@gmail.com>
2024-03-15 23:06:31 +0530

    Resolved conflicts in package.json

--- src/resources/Subs.tsx
+++ src/resources/Subs.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+  
 
 function subs() {
   return (
-    <div>subs</div>
+    <div>
+        <Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader >
+    <TableRow >
+      <TableHead className="w-[100px]">Subjects</TableHead>
+      <TableHead className="text-right">Lectures</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+
+    </div>
   )
 }
 
